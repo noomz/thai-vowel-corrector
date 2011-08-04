a3ea1266bd039aad48ea746b7ddcbcae81b773fa
Siriwat Uamngamsup <lazyM0m3nt@gmail.com>
2011-08-04 13:44:24 +0700

    Fix print media

--- cufon-thai.js
+++ cufon-thai.js
@@ -13,349 +13,349 @@
  * @return Modified text.
  */
 var correctThaiFloatingVowel = (function () {
-  
-  /**
-   * @return string from character code provided.
-   */
-  var _ = function (code) {
-    return String.fromCharCode(code);
-  };
-  
-  var
-    // Lower level characters
-    SARA_U  = _(0xE38),
-    SARA_UU = _(0xE39),
-    PHINTHU = _(0xE3A),
-
-    // Lower level characters after pullDown
-    SARA_U_DOWN  = _(0xF718),
-    SARA_UU_DOWN = _(0xF719),
-    PHINTHU_DOWN = _(0xF71A),
-
-    // Upper level 1 characters
-    MAI_HAN_AKAT = _(0xE31),
-    SARA_AM      = _(0xE33),
-    SARA_I       = _(0xE34),
-    SARA_Ii      = _(0xE35),
-    SARA_Ue      = _(0xE36),
-    SARA_Uee     = _(0xE37),
-    MAI_TAI_KHU  = _(0xE47),
-
-    // Upper level 1 characters after shift left
-    MAI_HAN_AKAT_LEFT_SHIFT = _(0xF710),
-    SARA_I_LEFT_SHIFT       = _(0xF701),
-    SARA_Ii_LEFT_SHIFT      = _(0xF702),
-    SARA_Ue_LEFT_SHIFT      = _(0xF703),
-    SARA_Uee_LEFT_SHIFT     = _(0xF704),
-    MAI_TAI_KHU_LEFT_SHIFT  = _(0xF712),
-
-    // Upper level 2 characters
-    MAI_EK      = _(0xE48),
-    MAI_THO     = _(0xE49),
-    MAI_TRI     = _(0xE4A),
-    MAI_CHATTAWA = _(0xE4B),
-    THANTHAKHAT = _(0xE4C),
-    NIKHAHIT    = _(0xE4D),
-
-    // Upper level 2 characters after pull down
-    MAI_EK_DOWN      = _(0xF70A),
-    MAI_THO_DOWN     = _(0xF70B),
-    MAI_TRI_DOWN     = _(0xF70C),
-    MAI_CHATTAWA_DOWN = _(0xF70D),
-    THANTHAKHAT_DOWN = _(0xF70E),
-
-    // Upper level 2 characters after pull down and shift left
-    MAI_EK_PULL_DOWN_AND_LEFT_SHIFT      = _(0xF705),
-    MAI_THO_PULL_DOWN_AND_LEFT_SHIFT     = _(0xF706),
-    MAI_TRI_PULL_DOWN_AND_LEFT_SHIFT     = _(0xF707),
-    MAI_CHATTAWA_PULL_DOWN_AND_LEFT_SHIFT = _(0xF708),
-    THANTHAKHAT_PULL_DOWN_AND_LEFT_SHIFT = _(0xF709),
-
-    // Upper level 2 characters after shift left
-    MAI_EK_LEFT_SHIFT       = _(0xF713),
-    MAI_THO_LEFT_SHIFT      = _(0xF714),
-    MAI_TRI_LEFT_SHIFT      = _(0xF715),
-    MAI_CHATTAWA_LEFT_SHIFT = _(0xF716),
-    THANTHAKHAT_LEFT_SHIFT  = _(0xF717),
-    NIKHAHIT_LEFT_SHIFT     = _(0xF711),
-
-    // Up tail characters
-    PO_PLA  = _(0x0E1B),
-    FO_FA   = _(0x0E1D),
-    FO_FAN  = _(0x0E1F),
-    LOchULA = _(0x0E2C),
-
-    // Down tail characters
-    THO_THAN = _(0xE10),
-    YO_YING  = _(0xE0D),
-    DOchADA  = _(0xE0E),
-    TO_PATAK = _(0xE0F),
-    RU       = _(0xE24),
-    LU       = _(0xE26),
-
-    // Cut tail characters
-    THO_THAN_CUT_TAIL = _(0xF700),
-    YO_YING_CUT_TAIL  = _(0xF70F),
-
-    // for exploded SARA_AM (NIKHAHIT + SARA_AA)
-    SARA_AA = _(0xE32);
-
-    var explodeSaraAm = function (content) {
-      var count = countSaraAm(content);
-
-      if (count === 0) {
-        return content;
-      }
-
-      var
-        newContent = '',
-        i = 0,
-        j = 0,
-        ch = '';
-
-      // Exploded SARA_AM to NIKHAHIT + SARA_AA
-      for (i = 0; i < content.length; i++) {
-        ch = content[i];
-        
-        if (i < content.length - 1  && content[i + 1] == SARA_AM) {
-          if (isUpperLevel2(ch)) {
-            newContent += NIKHAHIT;
-            newContent += ch;
-          }
-          else {
-            newContent += ch;
-            newContent += NIKHAHIT;
-          }
-        }
-        else if (ch == SARA_AM){
-          newContent += SARA_AA;
-        }
-        else {
-          newContent += ch;
-        }
-      }
-      
-      return newContent;
-    };
-
-    var countSaraAm = function(content) {
-      var
-        count = 0,
-        i = 0;
-
-      for (i = 0; i < content.length; i++) {
-        if (content[i] == SARA_AM) {
-          count++;
-        }
-      }
-
-      return count;
-    };
-
-    var isUpTail = function (ch) {
-      return ch == PO_PLA || ch == FO_FA || ch == FO_FAN
-             || ch == LOchULA;
-    };
-
-    var isDownTail = function (ch) {
-      return ch == THO_THAN || ch == YO_YING || ch == DOchADA
-             || ch == TO_PATAK || ch == RU || ch == LU;
-    };
-
-    var isUpperLevel1 = function (ch) {
-      return ch == MAI_HAN_AKAT || ch == SARA_I || ch == SARA_Ii
-             || ch == SARA_Ue || ch == SARA_Uee || ch == MAI_TAI_KHU
-             || ch == NIKHAHIT;
-    };
-
-    var isLeftShiftUpperLevel1 = function (ch) {
-      return ch == MAI_HAN_AKAT_LEFT_SHIFT || ch == SARA_I_LEFT_SHIFT
-             || ch == SARA_Ii_LEFT_SHIFT || ch == SARA_Ue_LEFT_SHIFT
-             || ch == SARA_Uee_LEFT_SHIFT || ch == MAI_TAI_KHU_LEFT_SHIFT
-             || ch == NIKHAHIT_LEFT_SHIFT;
-    };
-
-    var isUpperLevel2 = function (ch) {
-      return ch == MAI_EK || ch == MAI_THO || ch == MAI_TRI
-             || ch == MAI_CHATTAWA || ch == THANTHAKHAT;
-    };
-
-    var isLowerLevel = function (ch) {
-      return ch == SARA_U || ch == SARA_UU || ch == PHINTHU;
-    };
-
-    var pullDownAndShiftLeft = function (ch) {
-      switch (ch) {
-        case MAI_EK:
-          return MAI_EK_PULL_DOWN_AND_LEFT_SHIFT;
-
-        case MAI_THO:
-          return MAI_THO_PULL_DOWN_AND_LEFT_SHIFT;
-
-        case MAI_TRI:
-          return MAI_TRI_PULL_DOWN_AND_LEFT_SHIFT;
-
-        case MAI_CHATTAWA:
-          return MAI_CHATTAWA_PULL_DOWN_AND_LEFT_SHIFT;
-
-        case MAI_HAN_AKAT:
-          return MAI_HAN_AKAT_LEFT_SHIFT;
-
-        case THANTHAKHAT:
-          return THANTHAKHAT_PULL_DOWN_AND_LEFT_SHIFT;
-
-        default:
-          return ch;
-      }
-    };
-
-    var shiftLeft = function (ch) {
-      switch (ch) {
-        case MAI_EK:
-          return MAI_EK_LEFT_SHIFT;
-
-        case MAI_THO:
-          return MAI_THO_LEFT_SHIFT;
-
-        case MAI_TRI:
-          return MAI_TRI_LEFT_SHIFT;
-
-        case MAI_CHATTAWA:
-          return MAI_CHATTAWA_LEFT_SHIFT;
-
-        case MAI_HAN_AKAT:
-          return MAI_HAN_AKAT_LEFT_SHIFT;
-
-        case SARA_I:
-          return SARA_I_LEFT_SHIFT;
-
-        case SARA_Ii:
-          return SARA_Ii_LEFT_SHIFT;
-
-        case SARA_Ue:
-          return SARA_Ue_LEFT_SHIFT;
-
-        case SARA_Uee:
-          return SARA_Uee_LEFT_SHIFT;
-
-        case MAI_TAI_KHU:
-          return MAI_TAI_KHU_LEFT_SHIFT;
-
-        case NIKHAHIT:
-          return NIKHAHIT_LEFT_SHIFT;
-
-        default:
-          return ch;
-      }
-    };
-
-    var pullDown = function (ch) {
-      switch (ch) {
-        case MAI_EK:
-          return MAI_EK_DOWN;
-
-        case MAI_THO:
-          return MAI_THO_DOWN;
-
-        case MAI_TRI:
-          return MAI_TRI_DOWN;
-
-        case MAI_CHATTAWA:
-          return MAI_CHATTAWA_DOWN;
-
-        case THANTHAKHAT:
-          return THANTHAKHAT_DOWN;
-
-        case SARA_U:
-          return SARA_U_DOWN;
-
-        case SARA_UU:
-          return SARA_UU_DOWN;
-
-        case PHINTHU:
-          return PHINTHU_DOWN;
-
-        default:
-          return ch;
-      }
-    };
-
-    var cutTail = function (ch) {
-      switch(ch) {
-        case THO_THAN:
-          return THO_THAN_CUT_TAIL;
-
-        case YO_YING:
-          return YO_YING_CUT_TAIL;
-
-        default:
-          return ch;
-      }
-    };
-
-    /**
-     * Rearrange Thai glyph
-     * @param content
-     * @return string for display
-     */
-    var do_modify = function (content) {
-      content = explodeSaraAm(content || '');
-      content = content.split(''); // string cannot be modified in javascript,
-                                   // then convert it to array for convenient.
-
-      var
-        length = content.length,
-        pch = 'a'; // previous character start with dummy value
-
-      var
-        i = 0,
-        ch = '',
-        cutch = '';
-
-      // Replace upper and lower character with un-overlapped character
-      for (i = 0; i < length; i++) {
-        ch = content[i];
-
-        if (isUpperLevel1(ch) && isUpTail(pch)) { // Level 1 and up-tail
-          content[i] = shiftLeft(ch);
-        }
-        else if (isUpperLevel2(ch)) {
-          
-          // Level 2
-          if (isLowerLevel(pch)) {
-            pch = content[i - 2];
-          }
-
-          if (isUpTail(pch)) {
-            content[i] = pullDownAndShiftLeft(ch);
-          }
-          else if (isLeftShiftUpperLevel1(pch)) {
-            content[i] = shiftLeft(ch);
-          }
-          else if (!isUpperLevel1(pch)) {
-            content[i] = pullDown(ch);
-          }
-        }
-        else if (isLowerLevel(ch) && isDownTail(pch)) { // Lower level and down-tail
-          cutch = cutTail(pch);
-
-          if (pch != cutch) {
-            content[i - 1] = cutch;
-          }
-          else {
-            content[i] = pullDown(ch);
-          }
-        }
-
-        pch = content[i];
-      }
-
-      return content.join('');
-    };
-
-    return function (text) {
-      return do_modify(text);
-    };
+	
+	/**
+	 * @return string from character code provided.
+	 */
+	var _ = function (code) {
+		return String.fromCharCode(code);
+	};
+	
+	var
+		// Lower level characters
+		SARA_U  = _(0xE38),
+		SARA_UU = _(0xE39),
+		PHINTHU = _(0xE3A),
+
+		// Lower level characters after pullDown
+		SARA_U_DOWN  = _(0xF718),
+		SARA_UU_DOWN = _(0xF719),
+		PHINTHU_DOWN = _(0xF71A),
+
+		// Upper level 1 characters
+		MAI_HAN_AKAT = _(0xE31),
+		SARA_AM      = _(0xE33),
+		SARA_I       = _(0xE34),
+		SARA_Ii      = _(0xE35),
+		SARA_Ue      = _(0xE36),
+		SARA_Uee     = _(0xE37),
+		MAI_TAI_KHU  = _(0xE47),
+
+		// Upper level 1 characters after shift left
+		MAI_HAN_AKAT_LEFT_SHIFT = _(0xF710),
+		SARA_I_LEFT_SHIFT       = _(0xF701),
+		SARA_Ii_LEFT_SHIFT      = _(0xF702),
+		SARA_Ue_LEFT_SHIFT      = _(0xF703),
+		SARA_Uee_LEFT_SHIFT     = _(0xF704),
+		MAI_TAI_KHU_LEFT_SHIFT  = _(0xF712),
+
+		// Upper level 2 characters
+		MAI_EK      = _(0xE48),
+		MAI_THO     = _(0xE49),
+		MAI_TRI     = _(0xE4A),
+		MAI_CHATTAWA = _(0xE4B),
+		THANTHAKHAT = _(0xE4C),
+		NIKHAHIT    = _(0xE4D),
+
+		// Upper level 2 characters after pull down
+		MAI_EK_DOWN      = _(0xF70A),
+		MAI_THO_DOWN     = _(0xF70B),
+		MAI_TRI_DOWN     = _(0xF70C),
+		MAI_CHATTAWA_DOWN = _(0xF70D),
+		THANTHAKHAT_DOWN = _(0xF70E),
+
+		// Upper level 2 characters after pull down and shift left
+		MAI_EK_PULL_DOWN_AND_LEFT_SHIFT      = _(0xF705),
+		MAI_THO_PULL_DOWN_AND_LEFT_SHIFT     = _(0xF706),
+		MAI_TRI_PULL_DOWN_AND_LEFT_SHIFT     = _(0xF707),
+		MAI_CHATTAWA_PULL_DOWN_AND_LEFT_SHIFT = _(0xF708),
+		THANTHAKHAT_PULL_DOWN_AND_LEFT_SHIFT = _(0xF709),
+
+		// Upper level 2 characters after shift left
+		MAI_EK_LEFT_SHIFT       = _(0xF713),
+		MAI_THO_LEFT_SHIFT      = _(0xF714),
+		MAI_TRI_LEFT_SHIFT      = _(0xF715),
+		MAI_CHATTAWA_LEFT_SHIFT = _(0xF716),
+		THANTHAKHAT_LEFT_SHIFT  = _(0xF717),
+		NIKHAHIT_LEFT_SHIFT     = _(0xF711),
+
+		// Up tail characters
+		PO_PLA  = _(0x0E1B),
+		FO_FA   = _(0x0E1D),
+		FO_FAN  = _(0x0E1F),
+		LOchULA = _(0x0E2C),
+
+		// Down tail characters
+		THO_THAN = _(0xE10),
+		YO_YING  = _(0xE0D),
+		DOchADA  = _(0xE0E),
+		TO_PATAK = _(0xE0F),
+		RU       = _(0xE24),
+		LU       = _(0xE26),
+
+		// Cut tail characters
+		THO_THAN_CUT_TAIL = _(0xF700),
+		YO_YING_CUT_TAIL  = _(0xF70F),
+
+		// for exploded SARA_AM (NIKHAHIT + SARA_AA)
+		SARA_AA = _(0xE32);
+
+		var explodeSaraAm = function (content) {
+			var count = countSaraAm(content);
+
+			if (count === 0) {
+				return content;
+			}
+
+			var
+				newContent = '',
+				i = 0,
+				j = 0,
+				ch = '';
+
+			// Exploded SARA_AM to NIKHAHIT + SARA_AA
+			for (i = 0; i < content.length; i++) {
+				ch = content[i];
+				
+				if (i < content.length - 1  && content[i + 1] == SARA_AM) {
+					if (isUpperLevel2(ch)) {
+						newContent += NIKHAHIT;
+						newContent += ch;
+					}
+					else {
+						newContent += ch;
+						newContent += NIKHAHIT;
+					}
+				}
+				else if (ch == SARA_AM){
+					newContent += SARA_AA;
+				}
+				else {
+					newContent += ch;
+				}
+			}
+			
+			return newContent;
+		};
+
+		var countSaraAm = function(content) {
+			var
+				count = 0,
+				i = 0;
+
+			for (i = 0; i < content.length; i++) {
+				if (content[i] == SARA_AM) {
+					count++;
+				}
+			}
+
+			return count;
+		};
+
+		var isUpTail = function (ch) {
+			return ch == PO_PLA || ch == FO_FA || ch == FO_FAN
+						 || ch == LOchULA;
+		};
+
+		var isDownTail = function (ch) {
+			return ch == THO_THAN || ch == YO_YING || ch == DOchADA
+						 || ch == TO_PATAK || ch == RU || ch == LU;
+		};
+
+		var isUpperLevel1 = function (ch) {
+			return ch == MAI_HAN_AKAT || ch == SARA_I || ch == SARA_Ii
+						 || ch == SARA_Ue || ch == SARA_Uee || ch == MAI_TAI_KHU
+						 || ch == NIKHAHIT;
+		};
+
+		var isLeftShiftUpperLevel1 = function (ch) {
+			return ch == MAI_HAN_AKAT_LEFT_SHIFT || ch == SARA_I_LEFT_SHIFT
+						 || ch == SARA_Ii_LEFT_SHIFT || ch == SARA_Ue_LEFT_SHIFT
+						 || ch == SARA_Uee_LEFT_SHIFT || ch == MAI_TAI_KHU_LEFT_SHIFT
+						 || ch == NIKHAHIT_LEFT_SHIFT;
+		};
+
+		var isUpperLevel2 = function (ch) {
+			return ch == MAI_EK || ch == MAI_THO || ch == MAI_TRI
+						 || ch == MAI_CHATTAWA || ch == THANTHAKHAT;
+		};
+
+		var isLowerLevel = function (ch) {
+			return ch == SARA_U || ch == SARA_UU || ch == PHINTHU;
+		};
+
+		var pullDownAndShiftLeft = function (ch) {
+			switch (ch) {
+				case MAI_EK:
+					return MAI_EK_PULL_DOWN_AND_LEFT_SHIFT;
+
+				case MAI_THO:
+					return MAI_THO_PULL_DOWN_AND_LEFT_SHIFT;
+
+				case MAI_TRI:
+					return MAI_TRI_PULL_DOWN_AND_LEFT_SHIFT;
+
+				case MAI_CHATTAWA:
+					return MAI_CHATTAWA_PULL_DOWN_AND_LEFT_SHIFT;
+
+				case MAI_HAN_AKAT:
+					return MAI_HAN_AKAT_LEFT_SHIFT;
+
+				case THANTHAKHAT:
+					return THANTHAKHAT_PULL_DOWN_AND_LEFT_SHIFT;
+
+				default:
+					return ch;
+			}
+		};
+
+		var shiftLeft = function (ch) {
+			switch (ch) {
+				case MAI_EK:
+					return MAI_EK_LEFT_SHIFT;
+
+				case MAI_THO:
+					return MAI_THO_LEFT_SHIFT;
+
+				case MAI_TRI:
+					return MAI_TRI_LEFT_SHIFT;
+
+				case MAI_CHATTAWA:
+					return MAI_CHATTAWA_LEFT_SHIFT;
+
+				case MAI_HAN_AKAT:
+					return MAI_HAN_AKAT_LEFT_SHIFT;
+
+				case SARA_I:
+					return SARA_I_LEFT_SHIFT;
+
+				case SARA_Ii:
+					return SARA_Ii_LEFT_SHIFT;
+
+				case SARA_Ue:
+					return SARA_Ue_LEFT_SHIFT;
+
+				case SARA_Uee:
+					return SARA_Uee_LEFT_SHIFT;
+
+				case MAI_TAI_KHU:
+					return MAI_TAI_KHU_LEFT_SHIFT;
+
+				case NIKHAHIT:
+					return NIKHAHIT_LEFT_SHIFT;
+
+				default:
+					return ch;
+			}
+		};
+
+		var pullDown = function (ch) {
+			switch (ch) {
+				case MAI_EK:
+					return MAI_EK_DOWN;
+
+				case MAI_THO:
+					return MAI_THO_DOWN;
+
+				case MAI_TRI:
+					return MAI_TRI_DOWN;
+
+				case MAI_CHATTAWA:
+					return MAI_CHATTAWA_DOWN;
+
+				case THANTHAKHAT:
+					return THANTHAKHAT_DOWN;
+
+				case SARA_U:
+					return SARA_U_DOWN;
+
+				case SARA_UU:
+					return SARA_UU_DOWN;
+
+				case PHINTHU:
+					return PHINTHU_DOWN;
+
+				default:
+					return ch;
+			}
+		};
+
+		var cutTail = function (ch) {
+			switch(ch) {
+				case THO_THAN:
+					return THO_THAN_CUT_TAIL;
+
+				case YO_YING:
+					return YO_YING_CUT_TAIL;
+
+				default:
+					return ch;
+			}
+		};
+
+		/**
+		 * Rearrange Thai glyph
+		 * @param content
+		 * @return string for display
+		 */
+		var do_modify = function (content) {
+			content = explodeSaraAm(content || '');
+			content = content.split(''); // string cannot be modified in javascript,
+																	 // then convert it to array for convenient.
+
+			var
+				length = content.length,
+				pch = 'a'; // previous character start with dummy value
+
+			var
+				i = 0,
+				ch = '',
+				cutch = '';
+
+			// Replace upper and lower character with un-overlapped character
+			for (i = 0; i < length; i++) {
+				ch = content[i];
+
+				if (isUpperLevel1(ch) && isUpTail(pch)) { // Level 1 and up-tail
+					content[i] = shiftLeft(ch);
+				}
+				else if (isUpperLevel2(ch)) {
+					
+					// Level 2
+					if (isLowerLevel(pch)) {
+						pch = content[i - 2];
+					}
+
+					if (isUpTail(pch)) {
+						content[i] = pullDownAndShiftLeft(ch);
+					}
+					else if (isLeftShiftUpperLevel1(pch)) {
+						content[i] = shiftLeft(ch);
+					}
+					else if (!isUpperLevel1(pch)) {
+						content[i] = pullDown(ch);
+					}
+				}
+				else if (isLowerLevel(ch) && isDownTail(pch)) { // Lower level and down-tail
+					cutch = cutTail(pch);
+
+					if (pch != cutch) {
+						content[i - 1] = cutch;
+					}
+					else {
+						content[i] = pullDown(ch);
+					}
+				}
+
+				pch = content[i];
+			}
+
+			return content.join('');
+		};
+
+		return function (text) {
+			return do_modify(text);
+		};
 
 })();
 
@@ -1047,7 +1047,7 @@ var Cufon = (function() {
 		// with larger fontSize, however such cases are rare and can
 		// be fixed by using a more specific selector
 		if (parseFloat(style.get('fontSize')) === 0) return;
-		var font = getFont(el, style), node, type, next, anchor, text, lastElement;
+		var font = getFont(el, style), node, type, next, anchor, text, original_text, original_el, lastElement;
 		var isShy = options.softHyphens, anyShy = false, pos, shy, reShy = /\u00ad/g;
 		var modifyText = options.modifyText;
 		if (!font) return;
@@ -1077,13 +1077,24 @@ var Cufon = (function() {
 				if (next) continue;
 			}
 			if (anchor) {
-				text = anchor.data;
+				text = original_text = anchor.data;
 				if (!isShy) text = text.replace(reShy, '');
 				text = CSS.whiteSpace(text, style, anchor, lastElement, simple);
 				// modify text only on the first replace
 				if (modifyText) text = modifyText(text, anchor, el, options);
 				el.replaceChild(process(font, text, style, options, node, el), anchor);
 				anchor = null;
+
+				// Insert original text, for searching, printing etc.
+				original_el = document.createElement('div');
+				original_el.setAttribute('class', 'cufon-original-text');
+				//original_el.style.display = 'inline-block';
+				//original_el.style.textIndent = '-10000in';
+				//original_el.style.textAlign = 'left';
+				//original_el.style.width = '0px';
+				//original_el.style.height = '0px';
+				original_el.appendChild(document.createTextNode(original_text));
+				el.appendChild(original_el);
 			}
 			if (type == 1) {
 				if (node.firstChild) {
@@ -1205,7 +1216,7 @@ var Cufon = (function() {
 		modifyText: correctThaiFloatingVowel,
 		onAfterReplace: null,
 		onBeforeReplace: null,
-		printable: true,
+		printable: false,
 		selector: (
 				window.Sizzle
 			||	(window.jQuery && function(query) { return jQuery(query); }) // avoid noConflict issues
@@ -1344,6 +1355,7 @@ Cufon.registerEngine('vml', (function() {
 			'cufonshy.cufon-shy-disabled,.cufon-viewport-resizing cufonshy{display:none;}' +
 			'cufonglue{white-space:nowrap;display:inline-block;}' +
 			'.cufon-viewport-resizing cufonglue{white-space:normal;}' +
+			'.cufon-original-text{display:-moz-inline-box;display:inline-block;width:0;height:0;text-align:left;text-indent:-10000in;}' +
 			'a cufon{cursor:pointer}' + // ignore !important here
 		'}' +
 		'@media print{' +
@@ -1602,6 +1614,7 @@ Cufon.registerEngine('canvas', (function() {
 			'cufonshy.cufon-shy-disabled,.cufon-viewport-resizing cufonshy{display:none;}' +
 			'cufonglue{white-space:nowrap;display:inline-block;}' +
 			'.cufon-viewport-resizing cufonglue{white-space:normal;}' +
+			'.cufon-original-text{display:-moz-inline-box;display:inline-block;width:0;height:0;text-align:left;text-indent:-10000in;}' +
 		'}' +
 		'@media print{' +
 			'cufon{padding:0;}' + // Firefox 2
@@ -1647,7 +1660,7 @@ Cufon.registerEngine('canvas', (function() {
 
 		var redraw = (text === null);
 
-		if (redraw) text = node.getAttribute('alt');
+		if (redraw) text = node.getAttribute('alt');console.log(node);
 
 		var viewBox = font.viewBox;
 
